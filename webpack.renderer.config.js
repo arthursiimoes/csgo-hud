d12d1f58dd50b542c7037435820c3e6fe088047d
Arthur Simões <simoesarthur236@gmail.com>
2022-11-11 15:07:57 -0300

    master

---
+++ webpack.renderer.config.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.config')
+
+module.exports = merge(common, {
+	entry: [
+		'./src/renderer/app.js',
+		'./src/stylus/app.styl',
+	],
+	target: 'electron-renderer',
+})
